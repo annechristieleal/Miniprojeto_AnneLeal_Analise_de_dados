1db2497ea8c51b2258eccb3d3d442bcb6f21d662
Anne Leal <christie_leal@gmail.com>
2026-08-15 16:39:17 -0300

    Sprint 1(Importacao de dados)

---
+++ projeto.py
@@ -0,0 +1,19 @@
+
+
+
+import pandas as pd
+from IPython.display import display
+
+df = pd.read_csv("base_varejo.csv", sep=";")
+
+display(df.head())
+display(df.shape)
+display(type(df))
+
+# Carregar a base_varejo.csv com pandas e mostrar: número de registros,colunas e tipos de dados.
+
+
+    
+    
+
+
